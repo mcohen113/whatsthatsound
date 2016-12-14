0c8b327fcd2da2d252b0039e359066740e439310
mike cohen <mcohen113@msn.com>
2016-12-14 18:46:26 -0500

    preparing for merge

---
+++ samplesongs.js
@@ -0,0 +1,6 @@
+sample songs
+
+paranoid = ddddddddddddddddccccccccfc..d...ddddddddddddddddccccccccfc..d...d......dcgj.dah....
+
+axle off = d...f..d.dg.d.c.d...a..d.d5.a.f.d.a.k.dc.c .e.d.......d...f..d.dg.d.c.d...a..d.d5.a.f.d.a.k.dc.c .e.d.......
+
