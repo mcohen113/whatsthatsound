e36a68f286771294eb13d8d3acf6b63627031d78
mike cohen <mcohen113@msn.com>
2017-04-04 19:49:41 -0400

    heat appended

--- samplesongs.js
+++ samplesongs.js
@@ -15,6 +15,8 @@ h.hh.q.q.qi.g...h.hh.q.q.qi.....h.hh.q.q.qi.g...g.g.g.gh.h....
 
 full = d...f..d.dg.d.c.d...a..d.d5.a.f.d.a.k.dc.c .e.d.......d...f..d.dg.d.c.d...a..d.d5.a.f.d.a.k.dc.c .e.d.......dc.a.f....h.hh.q.q.qi.g...h.hh.q.q.qi.....h.hh.q.q.qi.g...g.g.g.gh.h....h.hh.q.q.qi.g...h.hh.q.q.qi.....h.hh.q.q.qi.g...g.g.g.gh.h....
 
-the heat is on = .ceg3d.ceg3d....ceg3d.ceg3d.....ceg3d.ceg3d....ceg3d.ceg3d.k.k
+the heat is on = .ceg3d.ceg3d....ceg3d.ceg3d.....ceg3d.ceg3d....ceg3d.ceg3d.k.k.ceg3d.ceg3d....ceg3d.ceg3d.k.k.ceg3d.ceg3d....ceg3d.ceg3d.k.k
 
 .ceg3d.ceg3d.k.k
+
+
